861358723bcccc1389eea6ce748450600b9f7444
superzbo <zbo2@qq.com>
2015-03-28 21:21:39 +0800

    add bootstrap

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328020707) do
+ActiveRecord::Schema.define(version: 20150328025139) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "title"
+    t.date     "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
